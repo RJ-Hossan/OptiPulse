27d1e666bc5a2997957362b05cbf944a1ae5c89c
proximaCent <sabikaftahee01@gmail.com>
2025-01-12 15:43:15 +0600

    collaborators

--- frontend/collaborators.py
+++ frontend/collaborators.py
@@ -9,11 +9,11 @@ def run():
 
     # Group 1
     st.subheader("Anomaly Detection")
-    st.text("1. Alice Johnson (ID: 101)")
-    st.text("2. Bob Williams (ID: 102)")
-    st.text("3. Emily Davis (ID: 103)")
-    st.text("4. Michael Brown (ID: 104)")
-    st.text("5. John Smith (ID: 105)")
+    st.text("1. Sabik Aftahee (ID: 1904024)")
+    st.text("2. Tareque MD Hanif (ID: 102)")
+    st.text("3. Nure Hadi (ID: 103)")
+    st.text("4. Mostak(ID: 104)")
+    st.text("5. Alif Sheikh (ID: 105)")
 
     # Group 2
     st.subheader("Customer Satisfaction Analysis")
@@ -25,11 +25,9 @@ def run():
 
     # Group 3
     st.subheader("Customer Segementation")
-    st.text("1. Kevin Harris (ID: 301)")
-    st.text("2. Laura Wilson (ID: 302)")
-    st.text("3. Steve Carter (ID: 303)")
-    st.text("4. Angela White (ID: 304)")
-    st.text("5. Paul Walker (ID: 305)")
+    st.text("1. Nabiha Fiza (ID: 301)")
+    st.text("2. Prithula saha (ID: 302)")
+    st.text("3. Nowshin (ID: 303)")
 
     st.markdown("""
     Each collaborator has played a vital role in making this project a success. Thank you for your hard work and dedication!
